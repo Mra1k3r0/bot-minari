907bf9f775f75b5b6189dbeea35987e24ef2fafb
MrAik3ro <61144642+mraikero-01@users.noreply.github.com>
2022-02-28 15:41:35 +0800

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-// const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
-const port = 5000
 
 app.get('/', (req, res) => res.send('YAHALLO!!'));
 
